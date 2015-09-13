3e183f58a7698bf0b13ec22c704244397159c451
NAKANO <jnakano@ml110.local.nakano.gr.jp>
2015-09-13 23:15:20 +0900

    fix stopping when filename has single quotation

--- windows/copy_ts.sh
+++ windows/copy_ts.sh
@@ -2,19 +2,14 @@
 set -x
 
 cd $(dirname $0)
-echo --- >> log.txt
-date >> log.txt
-echo $0 >> log.txt
-echo $1 >> log.txt
-echo $2 >> log.txt
-echo $3 >> log.txt
+date
+echo $1
+echo $2
+echo $3
 UNIX_PATH=$(cygpath -u "$3")
-echo $UNIX_PATH >> log.txt
 ORIG_FILE_NAME=$(basename "$UNIX_PATH")
-echo $ORIG_FILE_NAME >> log.txt
 FILE_NAME=$(date +%s)-$RANDOM.ts
-echo $FILE_NAME >> log.txt
-
+ls -l "$UNIX_PATH"
 mv "$UNIX_PATH" tmp/$FILE_NAME
 
 #scp -c arcfour -i /home/jnakano/.ssh/id_rsa_nopp_scp_ts_to_ml110 "tmp/$FILE_NAME" 192.168.0.4:tmp/ts2mp4/ts 2>&1 >> log.txt
@@ -28,7 +23,7 @@ _EOD
 echo "$ORIG_FILE_NAME" > "tmp/${FILE_NAME}.queue"
 echo $1 >> "tmp/${FILE_NAME}.queue"
 echo $2 >> "tmp/${FILE_NAME}.queue"
-scp -i /home/jnakano/.ssh/id_rsa_nopp_touch_ts_queue_at_ml110 "tmp/${FILE_NAME}.queue" 192.168.0.4:tmp/ts2mp4/queue 2>&1 >> log.txt
+scp -i /home/jnakano/.ssh/id_rsa_nopp_touch_ts_queue_at_ml110 "tmp/${FILE_NAME}.queue" 192.168.0.4:tmp/ts2mp4/queue 2>&1
 
 rm "tmp/${FILE_NAME}.queue"
 rm "tmp/$FILE_NAME"
--- windows/scp.bat
+++ windows/scp.bat
@@ -5,5 +5,6 @@ for %%f in (%*) do (
   echo %%f >> C:\TV\log.txt
 )
 
-bash -c '/home/jnakano/scripts/copy_ts.sh %1 %2 %3 2^>^&1 ^>^> /home/jnakano/scripts/copy_ts.log'
+set TS_FILE=%3
 
+bash -c '/home/jnakano/scripts/copy_ts.sh %1 %2 %TS_FILE:'=\'% 2^>^&1 ^>^> /home/jnakano/scripts/copy_ts.log'
