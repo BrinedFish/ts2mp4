ba0b036221f90c451d1960f6d4713ee149e6c0a9
NAKANO <jnakano@ml110.local.nakano.gr.jp>
2015-09-13 23:17:39 +0900

    change output dir, ffmpeg speed

--- convert.sh
+++ convert.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+OUT_DIR=/home/hatsune/public_html/tv
+
 # $1: queue file
 
 cd $(dirname $0)/../
@@ -20,8 +22,9 @@ echo $PROGRAM_OPT_V
 echo $PROGRAM_OPT_A
 
 # ffmpeg -i 2015-09-12\ 00：00\ \[ディスカバリー\]名車再生\!AMCペーサー\(二\).ts -map 0:p:340:0 -c:v libx264 -map 0:p:340:1 -c:a libfdk_aac test.mp4
-/home/jnakano/ffmpeg/bin/ffmpeg -y -i "ts/$TS_NAME" $PROGRAM_OPT_V -c:v libx264 -preset ultrafast -profile main -level 3.1 -s 1280x720 $PROGRAM_OPT_A -c:a libfdk_aac "mp4/$TMP_MP4_NAME"
-mv "mp4/$TMP_MP4_NAME" "/home/jnakano/public_html/ipad_movies/$MP4_NAME"
+/home/jnakano/ffmpeg/bin/ffmpeg -y -i "ts/$TS_NAME" $PROGRAM_OPT_V -c:v libx264 -preset veryfast -profile main -level 3.1 -s 1280x720 $PROGRAM_OPT_A -c:a libfdk_aac "mp4/$TMP_MP4_NAME"
+chmod 777 "mp4/$TMP_MP4_NAME"
+mv "mp4/$TMP_MP4_NAME" "$OUT_DIR/$MP4_NAME"
 
 mv "ts/$TS_NAME" ts_finished
 mv "queue/$QUEUE_NAME" queue_finished
